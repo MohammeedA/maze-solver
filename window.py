13c12b7d2a208ad135db078e096501553bb498b7
Mohammad Al-Sultani <mohammedalsultani@outlook.com>
2025-04-21 20:57:32 +0200

    move root protocol to ensure proper closing

--- window.py
+++ window.py
@@ -1,12 +1,13 @@
 from tkinter import Tk, BOTH, Canvas
 
-class Window():
+class Window(Tk):
     def __init__(self, width: int, height: int):
         self.__root = Tk()
         self.__root.title("Window")
         self.canvas = Canvas(self.__root, width=width, height=height)
         self.canvas.pack(fill=BOTH, expand=True)
         self.running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
     
     def redraw(self):
         # Redraw the canvas if needed
@@ -18,10 +19,8 @@ class Window():
         self.running = True
         while self.running:
             self.redraw()
-    
     def close(self):
         self.running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.__root.destroy()
 
     def on_resize(self, event):
